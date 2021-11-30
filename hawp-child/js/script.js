bf3437f336b9ef4ab1ec8f511f14fc349e4d7463
Evan Fraser <evan@hawpmedia.com>
2021-11-30 12:18:28 -0500

    Remove bg animation script because we never use it.

--- hawp-child/js/script.js
+++ hawp-child/js/script.js
@@ -178,41 +178,6 @@ jQuery(function($) {
 	};
 
 	/**
-	 * Move background left or right with mouse perspecitve.
-	 */
-	hawp.animateBackground = function() {
-		var lFollowX = 0,
-			lFollowY = 0,
-			x = 0,
-			y = 0,
-			friction = 1 / 120;
-
-		function moveBackground() {
-			x += (lFollowX - x) * friction;
-			y += (lFollowY - y) * friction;
-
-			translate = 'translate3d(' + x + 'px, ' + y + 'px, 0px) scale(1.1)';
-
-			$('.bg').css({
-			'-webit-transform': translate,
-			'-moz-transform': translate,
-			'transform': translate
-			});
-
-			window.requestAnimationFrame(moveBackground);
-		}
-
-		$(window).on('mousemove click', function(e) {
-			var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-			var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-			lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-			lFollowY = (10 * lMouseY) / 100;
-		});
-
-		moveBackground();
-	};
-
-	/**
 	 * Animate wp block columns.
 	 */
 	hawp.animateContent = function() {
@@ -262,7 +227,6 @@ jQuery(function($) {
 		hawp.materialDesignGform();
 		//hawp.targetBlankWarning();
 		hawp.animateContent();
-		//hawp.animateBackground();
 		//hawp.owlAnimations();
 		hawp.stickyHeader();
 	});
