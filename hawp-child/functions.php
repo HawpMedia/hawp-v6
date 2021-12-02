fb4579efc3320ee3571e3b626853701205fba2ae
Evan Fraser <evan@hawpmedia.com>
2021-12-02 11:22:29 -0500

    Change editor compiled sass file name from style-editor.css to compiled-editor.css and move the file meant for custom editor css to the main child directory

--- hawp-child/functions.php
+++ hawp-child/functions.php
@@ -19,8 +19,8 @@ foreach ($includes as $include) {
 
 //add_action('after_setup_theme', function() {
 	add_theme_support('editor-styles');
-	add_editor_style(get_stylesheet_directory_uri().'/css/admin/style-editor.css');
-	add_editor_style(get_stylesheet_directory_uri().'/css/admin/style-editor-custom.css');
+	add_editor_style(get_stylesheet_directory_uri().'/css/compiled-editor.css');
+	add_editor_style(get_stylesheet_directory_uri().'/style-editor.css');
 	add_theme_support('editor-color-palette', array(
 		array(
 			'name'  => 'White',
