96734c35ee447eb1f7f9c312a8a7e99933930710
Evan Fraser <evan@hawpmedia.com>
2022-03-25 23:59:42 -0400

    Replace array() function with shorthand array brackets

--- hawp/core/shortcodes.php
+++ hawp/core/shortcodes.php
@@ -13,18 +13,18 @@ class Hawp_Theme_Shortcodes {
 	 * Constructor.
 	 */
 	public function setup() {
-		add_shortcode('home', array($this, 'relative_home'));
-		add_shortcode('home_url', array($this, 'relative_home'));
-		add_shortcode('uploads', array($this, 'relative_uploads'));
-		add_shortcode('theme', array($this, 'relative_stylesheet'));
-		add_shortcode('logo', array($this, 'shortcode_logo'));
-		add_shortcode('svg', array($this, 'shortcode_svg'));
-		add_shortcode('year', array($this, 'shortcode_year'));
-		add_shortcode('custom_loop', array($this, 'shortcode_custom_loop'));
-		add_filter('the_content', array($this, 'do_custom_loop'), 2);
-		add_shortcode('content_cta', array($this, 'shortcode_content_cta'));
-		add_shortcode('sitemap', array($this, 'shortcode_sitemap'));
-		add_shortcode('menu', array($this, 'shortcode_menu'));
+		add_shortcode('home', [$this, 'relative_home']);
+		add_shortcode('home_url', [$this, 'relative_home']);
+		add_shortcode('uploads', [$this, 'relative_uploads']);
+		add_shortcode('theme', [$this, 'relative_stylesheet']);
+		add_shortcode('logo', [$this, 'shortcode_logo']);
+		add_shortcode('svg', [$this, 'shortcode_svg']);
+		add_shortcode('content_cta', [$this, 'shortcode_content_cta']);
+		add_shortcode('sitemap', [$this, 'shortcode_sitemap']);
+		add_shortcode('menu', [$this, 'shortcode_menu']);
+		add_shortcode('year', [$this, 'shortcode_year']);
+		add_shortcode('custom_loop', [$this, 'shortcode_custom_loop']);
+		add_filter('the_content', [$this, 'do_custom_loop'], 2);
 	}
 
 	/**
@@ -33,9 +33,9 @@ class Hawp_Theme_Shortcodes {
 	 * [home_url]
 	 */
 	public function relative_home($atts) {
-		$atts = shortcode_atts(array(
+		$atts = shortcode_atts([
 			'id' => ''
-		), $atts);
+		], $atts);
 
 		if ($atts['id'] && get_permalink($atts['id'])) {
 			$result = esc_url(get_permalink($atts['id']));
@@ -51,10 +51,10 @@ class Hawp_Theme_Shortcodes {
 	 * [uploads]
 	 */
 	public function relative_uploads($atts) {
-		$atts = shortcode_atts(array(
+		$atts = shortcode_atts([
 			'id' => '',
 			'svg' => 0,
-		), $atts);
+		], $atts);
 
 		if ($atts['id'] && wp_get_attachment_url($atts['id'])) {
 			$result = esc_url(wp_get_attachment_url($atts['id']));
@@ -79,6 +79,36 @@ class Hawp_Theme_Shortcodes {
 	}
 
 	/**
+	 * Shortcode: [logo].
+	 */
+	public function shortcode_logo($atts) {
+		$atts = shortcode_atts([
+			'format' => 'link',
+			'class' => '',
+			'size' => 'full',
+		], $atts);
+
+		$format = [
+			'link' => '<a href="'.home_url('/').'" rel="home" class="'.$atts['class'].'">%s</a>',
+			'wrap' => '<span class="'.$atts['class'].'">%s</span>',
+			'raw' => '%s',
+		];
+		if (!array_key_exists($atts['format'], $format)) {
+			$atts['format'] = 'wrap';
+		}
+
+		$img = '';
+		$logo = get_theme_option('logo');
+		if (!empty($logo)) {
+			$img = wp_get_attachment_image($logo, $atts['size'], false, [
+				'class' => (!empty($atts['class']) ? $atts['class'].'-img' : ''),
+				'alt' => get_bloginfo('name'),
+			]);
+		}
+		return sprintf($format[$atts['format']], $img);
+	}
+
+	/**
 	 * Shortcode: svg
 	 *
 	 * @todo	Loop throgh the svgs using a foreach as key
@@ -87,36 +117,121 @@ class Hawp_Theme_Shortcodes {
 	 * [svg id=""]
 	 */
 	public function shortcode_svg($atts) {
-		$atts = shortcode_atts(array(
+		$atts = shortcode_atts([
 			'id' => '',
-		), $atts);
+		], $atts);
 
-		$args = array(
+		$args = [
 			'id' => intval($atts['id']-1),
-		);
+		];
 
-		$option_keys = array(
+		$keys = [
 			'label' => 'options_'.get_theme_option_prefix().'svgs_'.($args['id']).'_'.get_theme_option_prefix().'label',
 			'svg' => 'options_'.get_theme_option_prefix().'svgs_'.($args['id']).'_'.get_theme_option_prefix().'svg',
-		);
+		];
 
-		$option_values = array(
-			'class' => get_option($option_keys['label']) ? 'wpsb-svg-'.preg_replace('/\s*/', '', strtolower(get_option($option_keys['label']))) : '',
-			'svg' => get_option($option_keys['svg']),
-		);
+		$vals = [
+			'class' => get_option($keys['label']) ? 'wpsb-svg-'.preg_replace('/\s*/', '', strtolower(get_option($keys['label']))) : '',
+			'svg' => get_option($keys['svg']),
+		];
 
-		$result = '<span class="wpsb-svg wpsb-svg-id-'.$args['id'].' '.$option_values['class'].'">'.$option_values['svg'].'</span>';
+		$result = '<span class="wpsb-svg wpsb-svg-id-'.$args['id'].' '.$vals['class'].'">'.$vals['svg'].'</span>';
 
+		return $result;
+	}
+
+	/**
+	 * Shortcode: [content_cta].
+	 *
+	 * Simple cta sentence for the bottom of content pages.
+	 */
+	public function shortcode_content_cta($atts) {
+		$content = get_theme_option('content_cta');
+		if (empty($content)) {
+			$content = '<p>Call [company] at [phone] for more information or to schedule an appointment.</p>';
+		}
+		return apply_filters('content_cta', do_shortcode($content));
+	}
+
+	/**
+	 * Shortcode: [sitemap].
+	 *
+	 * Returns sitemap for any post type.
+	 */
+	public function shortcode_sitemap($atts) {
+		$atts = shortcode_atts([
+			'post_type' => 'page',
+			'orderby' => 'title',
+			'order' => 'ASC',
+			'exclude' => '',
+			'show_title' => '',
+			'wrapper_class' => '',
+			'select' => '',
+			'select_placeholder' => '',
+		], $atts);
+
+		$posts = get_posts([
+			'post_type' => $atts['post_type'],
+			'orderby' => $atts['orderby'],
+			'order' => $atts['order'],
+			'exclude' => $atts['exclude'],
+			'posts_per_page' => -1,
+		]);
+
+		$args = [
+			'select_placeholder' => ($atts['select_placeholder'] ? $atts['select_placeholder'] : 'Choose a '.$atts['post_type'].'...'),
+		];
+
+		$result = '';
+		$result .= ($atts['show_title'] != 'false' ? '<h2>'.get_post_type_object($atts['post_type'])->labels->name.'</h2>' : '');
+		$result .= '<div class="hm-sitemap-wrapper hm-sitemap-wrapper-'.preg_replace('/\s+/', '-', strtolower($atts['post_type'])).' '.$atts['wrapper_class'].'">';
+		if ($atts['select'] == 'true') { // Select option
+			$result .= '<select class="hm-sitemap hm-sitemap-select hm-sitemap-'.preg_replace('/\s+/', '-', strtolower($atts['post_type'])).'" onchange="if (this.value) window.location.href=this.value">';
+			$result .= '<option value="">'.$args['select_placeholder'].'</option>';
+			foreach ($posts as $post) {
+				$result .= '<option value="'.get_the_permalink($post->ID).'">'.$post->post_title.'</option>';
+			}
+			$result .= '</select>';
+		} else { // Unordered list
+			$result .= '<ul class="hm-sitemap hm-sitemap-'.preg_replace('/\s+/', '-', strtolower($atts['post_type'])).'">';
+			foreach ($posts as $post) {
+				$result .= '<li><a href="'.get_the_permalink($post->ID).'" rel="bookmark" class="hm-sitemap-item">'.$post->post_title.'</a></li>';
+			}
+			$result .= '</ul>';
+		}
+		$result .= '</div>';
+
+		return $result;
+	}
+
+	/**
+	 * Shortcode: [menu]
+	 *
+	 * Returns a menu based on its name.
+	 */
+	public function shortcode_menu($atts) {
+		$atts = shortcode_atts([
+			'name' => '',
+			'wrapper_class' => '',
+		], $atts);
+
+		$args = [
+			'name' => preg_replace('/\s+/', '-', strtolower($atts['name'])),
+		];
+
+		$result = '<nav id="'.$args['name'].'-navigation" class="'.$atts['wrapper_class'].' '.$args['name'].'-navigation" role="navigation">';
+		$result .= wp_nav_menu(['menu' => $args['name'], 'echo' => false]);
+		$result .= '</nav>';
 		return $result;
 	}
 
 	/**
 	 * Shortcode: [year].
 	 */
-	public function shortcode_year($atts=array()) {
-		$atts = shortcode_atts(array(
+	public function shortcode_year($atts) {
+		$atts = shortcode_atts([
 			'since' => '',
-		), $atts);
+		], $atts);
 
 		if ($atts['since']) {
 			$result = date('Y') - floatval($atts['since']);
@@ -128,40 +243,10 @@ class Hawp_Theme_Shortcodes {
 	}
 
 	/**
-	 * Shortcode: [logo].
-	 */
-	public function shortcode_logo($atts=array()) {
-		$atts = shortcode_atts(array(
-			'format' => 'link',
-			'class' => '',
-			'size' => 'full',
-		), $atts);
-
-		$format = array(
-			'link' => '<a href="'.home_url('/').'" rel="home" class="'.$atts['class'].'">%s</a>',
-			'wrap' => '<span class="'.$atts['class'].'">%s</span>',
-			'raw' => '%s',
-		);
-		if (!array_key_exists($atts['format'], $format)) {
-			$atts['format'] = 'wrap';
-		}
-
-		$img = '';
-		$logo = get_theme_option('logo');
-		if (!empty($logo)) {
-			$img = wp_get_attachment_image($logo, $atts['size'], false, array(
-				'class' => (!empty($atts['class']) ? $atts['class'].'-img' : ''),
-				'alt' => get_bloginfo('name'),
-			));
-		}
-		return sprintf($format[$atts['format']], $img);
-	}
-
-	/**
 	 * Shortcode: [custom_loop].
 	 */
 	public function shortcode_custom_loop($atts, $content) {
-		$atts = shortcode_atts(array(
+		$atts = shortcode_atts([
 			'type'     => 'post',
 			'id'       => '',
 			'cat_type' => 'category_name',
@@ -174,12 +259,12 @@ class Hawp_Theme_Shortcodes {
 			'order'    => 'DESC',
 			'status'   => 'publish',
 			'size'     => 'medium',
-		), $atts);
+		], $atts);
 
 		$atts['include'] = !empty($atts['include']) ? array_map('trim',explode(',', $atts['include'])) : $atts['id'];
 		$atts['exclude'] = !empty($atts['exclude']) ? array_map('trim',explode(',', $atts['exclude'])) : '';
 
-		$args = array(
+		$args = [
 			'post_type'        => $atts['type'],
 			$atts['cat_type']  => $atts['cat'],
 			'meta_key'         => $atts['meta_key'],
@@ -189,7 +274,7 @@ class Hawp_Theme_Shortcodes {
 			'orderby'          => $atts['orderby'],
 			'order'            => $atts['order'],
 			'post_status'      => $atts['status'],
-		);
+		];
 
 		$posts = get_posts($args);
 
@@ -244,91 +329,6 @@ class Hawp_Theme_Shortcodes {
 			$content = str_replace($matches[0][$i], do_shortcode($matches[0][$i]), $content);
 		}
 		return $content;
-	}
-
-	/**
-	 * Shortcode: [content_cta].
-	 *
-	 * Simple cta sentence for the bottom of content pages.
-	 */
-	public function shortcode_content_cta($atts) {
-		$content = get_theme_option('content_cta');
-		if (empty($content)) {
-			$content = '<p>Call [company] at [phone] for more information or to schedule an appointment.</p>';
-		}
-		return apply_filters('content_cta', do_shortcode($content));
-	}
-
-	/**
-	 * Shortcode: [sitemap].
-	 *
-	 * Returns sitemap for any post type.
-	 */
-	public function shortcode_sitemap($atts) {
-		$atts = shortcode_atts(array(
-			'post_type' => 'page',
-			'orderby' => 'title',
-			'order' => 'ASC',
-			'exclude' => '',
-			'show_title' => '',
-			'wrapper_class' => '',
-			'select' => '',
-			'select_placeholder' => '',
-		), $atts);
-
-		$posts = get_posts(array(
-			'post_type' => $atts['post_type'],
-			'orderby' => $atts['orderby'],
-			'order' => $atts['order'],
-			'exclude' => $atts['exclude'],
-			'posts_per_page' => -1,
-		));
-
-		$args = array(
-			'select_placeholder' => ($atts['select_placeholder'] ? $atts['select_placeholder'] : 'Choose a '.$atts['post_type'].'...'),
-		);
-
-		$result = '';
-		$result .= ($atts['show_title'] != 'false' ? '<h2>'.get_post_type_object($atts['post_type'])->labels->name.'</h2>' : '');
-		$result .= '<div class="hm-sitemap-wrapper hm-sitemap-wrapper-'.preg_replace('/\s+/', '-', strtolower($atts['post_type'])).' '.$atts['wrapper_class'].'">';
-		if ($atts['select'] == 'true') { // Select option
-			$result .= '<select class="hm-sitemap hm-sitemap-select hm-sitemap-'.preg_replace('/\s+/', '-', strtolower($atts['post_type'])).'" onchange="if (this.value) window.location.href=this.value">';
-			$result .= '<option value="">'.$args['select_placeholder'].'</option>';
-			foreach ($posts as $post) {
-				$result .= '<option value="'.get_the_permalink($post->ID).'">'.$post->post_title.'</option>';
-			}
-			$result .= '</select>';
-		} else { // Unordered list
-			$result .= '<ul class="hm-sitemap hm-sitemap-'.preg_replace('/\s+/', '-', strtolower($atts['post_type'])).'">';
-			foreach ($posts as $post) {
-				$result .= '<li><a href="'.get_the_permalink($post->ID).'" rel="bookmark" class="hm-sitemap-item">'.$post->post_title.'</a></li>';
-			}
-			$result .= '</ul>';
-		}
-		$result .= '</div>';
-
-		return $result;
-	}
-
-	/**
-	 * Shortcode: [menu]
-	 *
-	 * Returns a menu based on its name.
-	 */
-	public function shortcode_menu($atts) {
-		$atts = shortcode_atts(array(
-			'name' => '',
-			'wrapper_class' => '',
-		), $atts);
-
-		$args = array(
-			'name' => preg_replace('/\s+/', '-', strtolower($atts['name'])),
-		);
-
-		$result = '<nav id="'.$args['name'].'-navigation" class="'.$atts['wrapper_class'].' '.$args['name'].'-navigation" role="navigation">';
-		$result .= wp_nav_menu(array('menu' => $args['name'], 'echo' => false));
-		$result .= '</nav>';
-		return $result;
 	}
 }
 
