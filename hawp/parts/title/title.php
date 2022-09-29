a47ce21cb27d58e37449da59283eebaa90e7446b
Evan Fraser <evan@hawpmedia.com>
2022-09-28 23:21:02 -0400

    add is_front_page check to breadcrumbs

--- hawp/parts/title/title.php
+++ hawp/parts/title/title.php
@@ -21,6 +21,6 @@ if (is_404()) {
 
 <h1 class="main-title entry-title"><?php echo $title; ?></h1>
 
-<?php if (function_exists('yoast_breadcrumb')) yoast_breadcrumb('<nav class="main-breadcrumbs">', '</nav>'); ?>
+<?php if (function_exists('yoast_breadcrumb') && !is_front_page()) yoast_breadcrumb('<nav class="main-breadcrumbs">', '</nav>'); ?>
 
-<?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
+<?php if (function_exists('rank_math_the_breadcrumbs') && !is_front_page()) rank_math_the_breadcrumbs(); ?>
