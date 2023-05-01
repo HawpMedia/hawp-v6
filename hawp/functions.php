4bc9146826e44d1b04025a3aed7c8ad1b24b7840
Evan Fraser <evan@hawpmedia.com>
2023-05-01 16:51:10 -0400

    Release v6.2.1

--- hawp/functions.php
+++ hawp/functions.php
@@ -14,7 +14,7 @@ class Hawp_Theme {
 
 	public static $theme = [
 		'name'                => 'Hawp Theme',
-		'version'             => '6.2.0',
+		'version'             => '6.2.1',
 		'file'                => __FILE__,
 		'textdomain'          => 'hawp',
 		'option_prefix'       => 'hawp_theme_',
