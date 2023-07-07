6c67e94d245d0a6919cf284f05b5336fae574697
Evan Fraser <evan@hawpmedia.com>
2023-07-07 12:58:02 -0500

    This was part of the theme class move

--- hawp/functions.php
+++ hawp/functions.php
@@ -3,78 +3,17 @@
 // Get the theme rolling!
 // ------------------------------------------
 
+// Define constants
 define('HM_PATH', get_template_directory());
 define('HM_URL', get_template_directory_uri());
 define('HMC_PATH', get_stylesheet_directory());
 define('HMC_URL', get_stylesheet_directory_uri());
 define('HM_THEME_VERSION', wp_get_theme(get_template())->get('Version'));
 
-if (!class_exists('Hawp_Theme')):
+// Include the theme
+require_once(HM_PATH.'/core/theme.php');
 
-class Hawp_Theme {
-
-	public static $theme = [
-		'name'                => 'Hawp Theme',
-		'version'             => HM_THEME_VERSION,
-		'file'                => __FILE__,
-		'textdomain'          => 'hawp',
-		'option_prefix'       => 'hawp_theme_',
-		'update_url'          => 'https://update.hawp.dev/themes/hawp-v6/',
-	];
-
-	/**
-	 * Constructor.
-	 */
-	public function setup() {
-		add_action('after_setup_theme', [$this, 'includes'], 1);
-	}
-
-	/**
-	 * Includes.
-	 */
-	public function includes() {
-
-		// Return null if ACF Pro is not active.
-		if (!class_exists('acf')) {
-			add_action('admin_notices', function() {
-				if (!class_exists('acf')) {
-					printf('<div class="error"><p>'.__('<strong>Advanced Custom Fields Pro</strong> is deactivated or does not exist. Please install and activate it to use the active theme', 'hawp').'</p></div>', PHP_VERSION);
-				}
-			});
-			return null;
-		}
-
-		// Loop through core folder and auto include files.
-		$includes = [
-			'functions',
-			'helpers',
-			'setup',
-			'admin',
-			'options',
-			'shortcodes',
-			'utilities',
-			'updater',
-		];
-		foreach ($includes as $include) {
-			require_once "core/{$include}.php";
-		}
-	}
-
+// Set the content width, this doesnt matter for WP 5.0+ sites
+if (!isset($content_width)) {
+	$content_width = 1400;
 }
-
-/**
- * Initialize Hawp_Theme class with a function.
- */
-function hawp_theme() {
-	global $hawp_theme;
-
-	// Instantiate only once.
-	if (!isset($hawp_theme)) {
-		$hawp_theme = new Hawp_Theme();
-		$hawp_theme->setup();
-	}
-	return $hawp_theme;
-}
-hawp_theme();
-
-endif; // class_exists check
